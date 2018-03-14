9d8bcf8a4a2ab85f43ffc47d4aa73e945dfef57a
tnguyen452 <tnntech@gatech.edu>
2018-03-14 00:08:49 -0400

    CheckPrime

---
+++ CheckPrime.py
@@ -0,0 +1,19 @@
+
+
+
+
+def isPrime(num):
+
+	if num > 1:
+		for i in range(2, num):
+			if (num % i) == 0 :
+				print("This number is not prime.")
+				break
+			else:
+				print("This number is prime")
+	else:
+		print("This number is not prime")
+		
+
+
+isPrime(407)
